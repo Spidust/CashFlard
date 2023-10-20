4b49f40895fd5fed5374478ae3f48087c8d0a092
Spidust <tranhaidangg2@gmail.com>
2023-10-20 19:03:42 +0700

    improve page notification performance

--- src/component/TopBar.jsx
+++ src/component/TopBar.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/css/TopBar.css";
 import GetCategorieName from "../utils/State/GetCategorieName";
 import { useLocation } from "react-router-dom";
 
 function TopBar() {
 	const location = useLocation();
-	let id = location.pathname.split("/")[1];
+	const [id, setId] = useState(location.pathname.split("/")[1]);
 
 	useEffect(() => {
-		id = location.pathname.split("/")[1];
+		setId(location.pathname.split("/")[1]);
 	}, [location]);
 	return (
 		<div className="top-bar bar">
