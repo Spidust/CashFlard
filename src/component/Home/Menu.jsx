e6940721eb5f1b9aa74ac8b0b473953af02fd681
Spidust <tranhaidangg2@gmail.com>
2024-01-15 13:30:59 +0700

    ...

--- src/component/Home/Menu.jsx
+++ src/component/Home/Menu.jsx
@@ -6,13 +6,20 @@ function Menu(props) {
 	return (
 		<div className={"menu " + (props.active ? "active" : "")}>
 			<FaTimes className="quit" onClick={props.quit}></FaTimes>
-
 			<div className="menu__item">
 				<a href="https://github.com/Spidust/CashFlard">Github Dự Án</a>
 			</div>
 			<div className="menu__item">
 				<a href="https://www.facebook.com/profile.php?id=100085815830663">
 					Liên hệ facebook cá nhân
+				</a>
+			</div>
+			<div className="menu__item">
+				<a href="/soan-de">Trang Soạn đề</a>
+			</div>
+			<div className="menu__item">
+				<a href="https://github.com/Spidust/SoanDeCashflard">
+					Github trang Soạn đề
 				</a>
 			</div>
 		</div>
