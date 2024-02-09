7ce864c1a622178058b2362837fcd5b6a90916d5
Spidust <tranhaidangg2@gmail.com>
2024-02-09 08:36:21 +0700

    feat: menu

--- src/component/Home/Menu.jsx
+++ src/component/Home/Menu.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaFolderPlus } from "react-icons/fa";
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { FaFacebookF } from "react-icons/fa6";
 import "../../assets/css/home/Menu.css";
 
 function Menu(props) {
@@ -7,19 +9,25 @@ function Menu(props) {
 		<div className={"menu " + (props.active ? "active" : "")}>
 			<FaTimes className="quit" onClick={props.quit}></FaTimes>
 			<div className="menu__item">
-				<a href="https://github.com/Spidust/CashFlard">Github Dự Án</a>
+				<a href="https://github.com/Spidust/CashFlard">
+					<TbBrandGithubFilled size={20} /> Github Dự Án
+				</a>
 			</div>
 			<div className="menu__item">
 				<a href="https://www.facebook.com/profile.php?id=100085815830663">
+					<FaFacebookF size={20} />
 					Liên hệ facebook cá nhân
 				</a>
 			</div>
 			<div className="menu__item">
-				<a href="/soan-de">Trang Soạn đề</a>
+				<a href="/soan-de">
+					<FaFolderPlus size={20} />
+					Trang Soạn đề
+				</a>
 			</div>
 			<div className="menu__item">
 				<a href="https://github.com/Spidust/SoanDeCashflard">
-					Github trang Soạn đề
+					<TbBrandGithubFilled size={20} /> Github trang Soạn đề
 				</a>
 			</div>
 		</div>
