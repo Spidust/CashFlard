3c0b38a25ecefff3448ae7051a304cb5bd4cd144
admin <tranhaidangg2@gmail.com>
2024-04-14 00:34:35 -0700

    add quit to sync with client

--- src/component/Modal/SyncModal.jsx
+++ src/component/Modal/SyncModal.jsx
@@ -10,9 +10,9 @@ function SyncModal(props) {
             <div className="text-white">Dữ liệu của bạn và máy không đồng bộ, bạn muốn sử dụng dữ liệu:</div>
 
             <div className="btn-server p-3 bg-secondary hover:bg-primary rounded-md cursor-pointer mt-2" onClick={() => { props.SyncWithServer(); props.quit() }}>Máy chủ</div>
-            <div className="btn-client p-3 bg-secondary hover:bg-primary rounded-md cursor-pointer mt-2" onClick={() => { props.SyncWithClient(); }}>Máy khách(máy bạn đó)</div>
+            <div className="btn-client p-3 bg-secondary hover:bg-primary rounded-md cursor-pointer mt-2" onClick={() => { props.SyncWithClient(); props.quit()}}>Máy khách(máy bạn đó)</div>
             hoặc
-            <div className="logout p-3 bg-[red] rounded-md cursor-pointer" onClick={() => dispatch(setToken("   "))}>Đăng xuất</div>
+            <div className="logout p-3 bg-[red] rounded-md cursor-pointer" onClick={() => {dispatch(setToken("   ")); props.quit()}}>Đăng xuất</div>
         </div>
     )
 }
